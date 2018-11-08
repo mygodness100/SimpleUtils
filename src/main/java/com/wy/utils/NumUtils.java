d72ef0e77cc1369677955b7b5d2651205efc7792
zjy <1160704409@qq.com>
2018-11-08 10:48:26 +0800

    fix

--- src/main/java/com/wy/utils/NumUtils.java
+++ src/main/java/com/wy/utils/NumUtils.java
@@ -8,61 +8,62 @@ import java.util.Arrays;
 import java.util.Locale;
 
 public class NumUtils {
-	
+
 	private NumUtils() {
-		
+
 	}
-	
+
 	/**
 	 * 保留多位小数
-	 * @param num		要保留小数的原始值
-	 * @param retain	需要保留几位小数
+	 * @param num 要保留小数的原始值
+	 * @param retain 需要保留几位小数
 	 */
-	public static double retainDecimal(double num,int retain) {
+	public static double retainDecimal(double num, int retain) {
 		BigDecimal big = new BigDecimal(num);
-		BigDecimal result = big.setScale(retain,RoundingMode.HALF_UP);//四舍五入
+		BigDecimal result = big.setScale(retain, RoundingMode.HALF_UP);// 四舍五入
 		return Double.parseDouble(result.toString());
 	}
-	
+
 	/**
 	 * double类型避免精度缺失的加法运算
 	 */
 	public static Double add(double... ds) {
-		if(ds == null || ds.length == 0) {
+		if (ds == null || ds.length == 0) {
 			return null;
 		}
 		BigDecimal result = new BigDecimal(ds[0]);
-		for(int i=1;i<ds.length;i++) {
+		for (int i = 1; i < ds.length; i++) {
 			result = result.add(new BigDecimal(ds[i]));
 		}
 		return result.doubleValue();
 	}
-	
+
 	/**
 	 * 将数字转化为指定的字符串格式
 	 * @param dou 指定的double数字
-	 * @param format 需要进行转换的格式,若是需要格式固定,则使用0占位符,num超出的整数部分会全部显示,但是不足的会补0
-	 * num超出的小数部分为四舍五入,若是不足则补0
-	 * example 3.4555,进行0.000kg格式化后为3.456kg,进行#.###kg为3.456kg;
-	 * 34.4555进行0.000格式化为34.456,进行#.###为34.456;
-	 * 34.455记性000.0000格式化为034.4550,进行###.####格式化为34.455
+	 * @param format
+	 *            需要进行转换的格式,若是需要格式固定,则使用0占位符,num超出的整数部分会全部显示,但是不足的会补0
+	 *            num超出的小数部分为四舍五入,若是不足则补0 example
+	 *            3.4555,进行0.000kg格式化后为3.456kg,进行#.###kg为3.456kg;
+	 *            34.4555进行0.000格式化为34.456,进行#.###为34.456;
+	 *            34.455记性000.0000格式化为034.4550,进行###.####格式化为34.455
 	 */
-	public static String double2Str(double num,String format) {
+	public static String double2Str(double num, String format) {
 		DecimalFormat decimalFormat = new DecimalFormat(format);
-		return decimalFormat.format(num);//会进行四舍五入
+		return decimalFormat.format(num);// 会进行四舍五入
 	}
-	
+
 	/**
 	 * 将数值转换为当前系统默认的货币形式
 	 */
 	public static String getLocalCurrency(Number num) {
-		return getLocalCurrency(num,Locale.getDefault());
+		return getLocalCurrency(num, Locale.getDefault());
 	}
-	
+
 	/**
 	 * 将数值转换为指定格式的货币形式
 	 */
-	public static String getLocalCurrency(Number num,Locale locale) {
+	public static String getLocalCurrency(Number num, Locale locale) {
 		NumberFormat nf = NumberFormat.getCurrencyInstance(locale);
 		return nf.format(num);
 	}
@@ -72,9 +73,9 @@ public class NumUtils {
 	 */
 	public static final long getMax(long[] arrs) {
 		Arrays.sort(arrs);
-		return arrs[arrs.length-1];
+		return arrs[arrs.length - 1];
 	}
-	
+
 	/**
 	 * 获取数组中最小值
 	 */
@@ -82,15 +83,15 @@ public class NumUtils {
 		Arrays.sort(arrs);
 		return arrs[0];
 	}
-	
+
 	/**
 	 * 获取数组中最大值
 	 */
 	public static final int getMax(int[] arrs) {
 		Arrays.sort(arrs);
-		return arrs[arrs.length-1];
+		return arrs[arrs.length - 1];
 	}
-	
+
 	/**
 	 * 获取数组中最小值
 	 */
@@ -98,15 +99,15 @@ public class NumUtils {
 		Arrays.sort(arrs);
 		return arrs[0];
 	}
-	
+
 	/**
 	 * 获取数组中最大值
 	 */
 	public static final int getMax(short[] arrs) {
 		Arrays.sort(arrs);
-		return arrs[arrs.length-1];
+		return arrs[arrs.length - 1];
 	}
-	
+
 	/**
 	 * 获取数组中最小值
 	 */
@@ -114,15 +115,15 @@ public class NumUtils {
 		Arrays.sort(arrs);
 		return arrs[0];
 	}
-	
+
 	/**
 	 * 获取数组中最大值
 	 */
 	public static final double getMax(double[] arrs) {
 		Arrays.sort(arrs);
-		return arrs[arrs.length-1];
+		return arrs[arrs.length - 1];
 	}
-	
+
 	/**
 	 * 获取数组中最小值
 	 */
@@ -130,15 +131,15 @@ public class NumUtils {
 		Arrays.sort(arrs);
 		return arrs[0];
 	}
-	
+
 	/**
 	 * 获取数组中最大值
 	 */
 	public static final double getMax(float[] arrs) {
 		Arrays.sort(arrs);
-		return arrs[arrs.length-1];
+		return arrs[arrs.length - 1];
 	}
-	
+
 	/**
 	 * 获取数组中最小值
 	 */
@@ -146,7 +147,7 @@ public class NumUtils {
 		Arrays.sort(arrs);
 		return arrs[0];
 	}
-	
+
 	/**
 	 * 数字类最小值,number中可以存放任何数字类型,byte,short,int,double,float,long等
 	 */
@@ -154,28 +155,83 @@ public class NumUtils {
 		Arrays.sort(nums);
 		return nums[0];
 	}
-	
+
 	/**
 	 * 数字类最大值
 	 */
 	public static Number getMax(Number[] nums) {
 		Arrays.sort(nums);
-		return nums[nums.length-1];
+		return nums[nums.length - 1];
 	}
-	
+
 	/**
 	 * 冒泡排序,默认升序
 	 */
-	public static int[] bubbleSort(int[] arrs,boolean asc) {
-		for(int i=1;i<arrs.length;i++){
-			for(int j=0;j<arrs.length - i;j++) {
-				if(arrs[j] > arrs[j+1]) {
-					int temp = arrs[j];
-					arrs[j] = arrs[j+1];
-					arrs[j+1] = temp;
+	public static int[] bubbleSort(int[] arrs) {
+		return bubbleSort(arrs, true);
+	}
+
+	/**
+	 * 冒泡排序,默认升序
+	 */
+	public static int[] bubbleSort(int[] arrs, boolean asc) {
+		for (int i = 1; i < arrs.length; i++) {
+			for (int j = 0; j < arrs.length - i; j++) {
+				if (asc) {
+					if (arrs[j] > arrs[j + 1]) {
+						swap(arrs, j, j + 1);
+					}
+				} else {
+					if (arrs[j] < arrs[j + 1]) {
+						swap(arrs, j, j + 1);
+					}
 				}
 			}
 		}
 		return arrs;
+	}
+
+	/**
+	 * 选择排序,默认升序
+	 * @param arrs 需要排序的数组
+	 * @return 排序后数组
+	 */
+	public static int[] selectSort(int[] arrs) {
+		return selectSort(arrs, true);
+	}
+
+	/**
+	 * 选择排序
+	 * @param arrs 需要排序的数组
+	 * @param asc 升序还是降序,默认true升序
+	 * @return 排序后数组
+	 */
+	public static int[] selectSort(int[] arrs, boolean asc) {
+		for (int i = 0; i < arrs.length; i++) {
+			for (int j = i + 1; j < arrs.length; j++) {
+				if (asc) {
+					if (arrs[i] > arrs[j]) {
+						swap(arrs, i, j);
+					}
+				} else {
+					if (arrs[i] < arrs[j]) {
+						swap(arrs, i, j);
+					}
+				}
+			}
+		}
+		return arrs;
+	}
+
+	/**
+	 * 数组数据交换
+	 * @param arr 数组
+	 * @param x 数组下标
+	 * @param y 数组下标
+	 */
+	public static void swap(int[] arr, int x, int y) {
+		int temp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = temp;
 	}
 }
