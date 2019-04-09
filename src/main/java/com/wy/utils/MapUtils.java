94b7e33a8fd244d08371dc6aa76b43a554e8d939
ParadiseWY <582822832@qq.com>
2019-04-09 17:39:39 +0800

    add utils

--- src/main/java/com/wy/utils/MapUtils.java
+++ src/main/java/com/wy/utils/MapUtils.java
@@ -1,5 +1,11 @@
 package com.wy.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,6 +16,30 @@ public class MapUtils {
 
 	public static <T, K> boolean isNotBlank(Map<T, K> map) {
 		return !isBlank(map);
+	}
+
+	public static String createParams(Map<String, Object> params, String charset) {
+		List<String> list = new ArrayList<>();
+		for (String key : params.keySet()) {
+			try {
+				list.add(URLEncoder.encode(key + "=" + params.get(key),
+						StandardCharsets.UTF_8.displayName()));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		}
+		return String.join("&", list);
+	}
+
+	public static String createParams(String url, Map<String, Object> params) {
+		return createParams(url, params, StandardCharsets.UTF_8.displayName());
+	}
+
+	public static String createParams(String url, Map<String, Object> params, String charset) {
+		if (isBlank(params)) {
+			return url;
+		}
+		return MessageFormat.format("{0}?{1}", url, createParams(params, charset));
 	}
 
 	public static MapBuilder getBuilder(String key, Object val) {
