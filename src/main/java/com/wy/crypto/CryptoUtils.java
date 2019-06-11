f7a68399999ea4f573f007f21befa66581659a0c
ParadiseWY <582822832@qq.com>
2019-06-11 17:52:20 +0800

    fix

--- src/main/java/com/wy/crypto/CryptoUtils.java
+++ src/main/java/com/wy/crypto/CryptoUtils.java
@@ -1,19 +1,37 @@
 package com.wy.crypto;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.UUID;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 import com.wy.utils.HexUtils;
 import com.wy.utils.StrUtils;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CryptoUtils {
 	/**
 	 * 按照UUID算法生成一个字符串
@@ -120,6 +138,106 @@ public class CryptoUtils {
 			// 9.根据密码器的初始化方式--加密/解密
 			return cip.doFinal(content);
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * 从字符串中加载公钥
+	 * @param publicKey 公钥字符串
+	 */
+	public static final RSAPublicKey loadPublicKey(String publicKey) {
+		byte[] buffer = Base64.getDecoder().decode(publicKey);
+		try {
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+			return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			log.error("算法错误或公钥非法");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * 从字符串中加载私钥
+	 * @param privateKeyStr 私钥字符串
+	 */
+	public static RSAPrivateKey loadPrivateKey(String privateKeyStr) {
+		byte[] buffer = Base64.getDecoder().decode(privateKeyStr);
+		try {
+			PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			log.error("算法错误或私钥非法");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * RSA公钥加密
+	 * @param key
+	 * @param xmlstr
+	 *            加密内容长度受秘钥长度限制，若加密内容长度大于(秘钥长度(1024)/8-11=117),
+	 *            则需要分段加密
+	 */
+	public static String RsaEncrypt(PublicKey key, String xmlstr) {
+		byte[] plainText = xmlstr.getBytes(StandardCharsets.UTF_8);
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			int inputLen = plainText.length;
+			int offSet = 0;
+			for (int i = 0; inputLen - offSet > 0; offSet = i * 116) {
+				byte[] cache;
+				if (inputLen - offSet > 116) {
+					cache = cipher.doFinal(plainText, offSet, 116);
+				} else {
+					cache = cipher.doFinal(plainText, offSet, inputLen - offSet);
+				}
+				out.write(cache, 0, cache.length);
+				++i;
+			}
+			return Base64.getEncoder().encodeToString(out.toByteArray());
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
+				| IllegalBlockSizeException | BadPaddingException | IOException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * RSA私钥解密
+	 * @param key 私钥
+	 * @param encodedText 若是分段加密，则需要分段解密
+	 */
+	public static String RsaDecrypt(PrivateKey key, String encodedText) {
+		// base64编码规定一行字符串不能超过76个,超过换行,换行符会导致编码失败
+		encodedText = encodedText.replaceAll("\r|\n", "");
+		byte[] textb = Base64.getDecoder().decode(encodedText);
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			int inputLen = textb.length;
+			int offSet = 0;
+			for (int i = 0; inputLen - offSet > 0; offSet = i * 128) {
+				byte[] cache;
+				if (inputLen - offSet > 128) {
+					cache = cipher.doFinal(textb, offSet, 128);
+				} else {
+					cache = cipher.doFinal(textb, offSet, inputLen - offSet);
+				}
+				out.write(cache, 0, cache.length);
+				++i;
+			}
+			return new String(out.toByteArray(), StandardCharsets.UTF_8);
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
+				| IllegalBlockSizeException | BadPaddingException | IOException e) {
+			log.error(e.getMessage());
 			e.printStackTrace();
 		}
 		return null;
