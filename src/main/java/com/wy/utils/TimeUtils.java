b2c0d05c4b679b83daa8285f02793998712318dd
ParadiseWY <582822832@qq.com>
2019-08-31 17:12:55 +0800

    fix

--- src/main/java/com/wy/utils/TimeUtils.java
+++ src/main/java/com/wy/utils/TimeUtils.java
@@ -4,21 +4,35 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * @description 基于jdk1.8的时间类工具类,和DateUtils差不多
+ * @apiNote 基于jdk1.8的时间类工具类,和DateUtils差不多
  * @author ParadiseWY
  * @date 2019年3月20日 下午4:56:06
- * @git {@link https://github.com/mygodness100}
  */
 public class TimeUtils {
 	private static final DateTimeFormatter formatter = DateTimeFormatter
 			.ofPattern(DateUtils.DEFAULT_PATTERN);
 
+	public static void main(String[] args) {
+		System.out.println(format(LocalDateTime.now()));
+		System.out.println(parse("2013-11-11T13:12:12"));
+	}
+
 	/**
 	 * FIXME
+	 * 
 	 * @param temporal
 	 * @return
 	 */
-	public static String format(LocalDateTime temporal) {
-		return formatter.format(temporal);
+	public static String format(LocalDateTime localDateTime) {
+		return formatter.format(localDateTime);
+	}
+
+	public static LocalDateTime parse(String date) {
+		// TemporalAccessor parse = formatter.parse(date);
+		formatter.parse(date, t -> {
+			System.out.println(t);
+			return LocalDateTime.now();
+		});
+		return LocalDateTime.parse(date);
 	}
 }
