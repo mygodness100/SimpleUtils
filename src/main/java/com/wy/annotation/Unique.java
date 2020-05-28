58bf9464404578564b48a2c1659b2cbe4dafcaa0
ParadiseWY <582822832@qq.com>
2020-05-28 17:29:23 +0800

    create

--- src/main/java/com/wy/annotation/Unique.java
+++ src/main/java/com/wy/annotation/Unique.java
@@ -22,4 +22,13 @@ public @interface Unique {
 	 * 当value为""时,取实体类字段的名称,实体类是否需要转换为蛇形
 	 */
 	boolean hump2Snake() default true;
+
+	/**
+	 * 当唯一字段进行更新时,前端需要将更新前的该字段的原始值重新传过来,该方法接收原始值
+	 * 1.当该参数有值时,根据反射取该参数在实体类中对应的值,若为null,抛异常
+	 * 2.当该值为null时,将在该注解的字段前添加ori并变成驼峰来查找实体类中的值,如username将变成oriUsername查找
+	 * 
+	 * @return 修改时的原始值的java接收属性名,该属性名必须在实体类中真实存在
+	 */
+	String oriName() default "";
 }
