e62b412c5fe42fed5b6b39ba3de7e4e953787796
ParadiseWY <582822832@qq.com>
2020-12-03 11:39:15 +0800

    study something

--- gitPush.sh
+++ gitPush.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 COMMENT="study something"
+
+# modify comment
 if [ $# -eq 1 ];then
         COMMENT=$1
 fi
+
+# is this repository have something to commit
+LABEL="working tree clean"
+GIT_STATUS=`git status|grep "$LABEL"`
+if [ "$GIT_STATUS" != "" ];then
+	echo -e "\033[1;32m----- nothing to commit and push to remote -----\033[0m"
+	exit 1
+fi
+
 echo -e "\033[1;32m----- pull request -----\033[0m"
 git pull
 echo ""
@@ -15,13 +26,13 @@ echo -e "\033[1;32m----- commit to local repository -----\033[0m"
 git commit -am "${COMMENT}"
 echo ""
 
-echo -e "\033[1;32m----- commit to gitee remote repository -----\033[0m"
+echo -e "\033[1;32m----- commit to github remote repository -----\033[0m"
 git push
 echo ""
 
-echo -e "\033[1;32m----- commit to github remote repository -----\033[0m"
-git push github master
-echo ""
+#echo -e "\033[1;32m----- commit to github remote repository -----\033[0m"
+#git push github master
+#echo ""
 
 echo -e "\033[1;32m----- check status -----\033[0m"
 git status
