270f3a31e60570c35696aa1d49488fa43d288485
ParadiseWY <582822832@qq.com>
2020-12-11 17:28:16 +0800

    study something

--- src/main/java/com/wy/utils/StrUtils.java
+++ src/main/java/com/wy/utils/StrUtils.java
@@ -9,11 +9,9 @@ import com.wy.enums.RegexEnum;
 import com.wy.result.ResultException;
 
 /**
- * 字符串帮助类 正则表达式的分组: @ (),每一个对括号表示一个分组,分组的顺序是从左括号出现的顺序, @
- * $后面加一个数字表示对应某一个分组,替代分组中不可直接表达的字符串
+ * 字符串帮助类 正则表达式的分组: @ (),每一个对括号表示一个分组,分组的顺序是从左括号出现的顺序, @ $后面加一个数字表示对应某一个分组,替代分组中不可直接表达的字符串
  * 
- * @example 快快乐乐 去掉叠词为快乐,pattern((.)\\1+,$1):
- *          第一个括号表示第一个分组中的任意字符;\\1表示第一个分组,\\2表示第2个分组,需要紧跟在分组之后
+ * @example 快快乐乐 去掉叠词为快乐,pattern((.)\\1+,$1): 第一个括号表示第一个分组中的任意字符;\\1表示第一个分组,\\2表示第2个分组,需要紧跟在分组之后
  *          +表示可出现多个相同的任意字符,$1表示将分组中的.所代表的任意字符串替换到$1,$2则表示替换第二个分组
  * @author wanyang 2018年7月7日
  */
@@ -23,8 +21,7 @@ public class StrUtils extends StringUtils {
 
 	private static final Pattern humpPattern = Pattern.compile("[A-Z]");
 
-	private StrUtils() {
-	}
+	private StrUtils() {}
 
 	public static boolean isNotBlank(CharSequence... args) {
 		for (CharSequence arg : args) {
@@ -44,6 +41,9 @@ public class StrUtils extends StringUtils {
 
 	/**
 	 * 将目标字符串的首字母变成大写
+	 * 
+	 * @param src 需要进行转换的字符串
+	 * @return 转换后的字符串
 	 */
 	public static String upperFirst(String src) {
 		return changeFirst(src, true);
@@ -51,11 +51,21 @@ public class StrUtils extends StringUtils {
 
 	/**
 	 * 将目标字符串的首字母变成小写
+	 * 
+	 * @param src 需要进行转换的字符串
+	 * @return 转换后的字符串
 	 */
 	public static String lowerFirst(String src) {
 		return changeFirst(src, false);
 	}
 
+	/**
+	 * 将目标字符串的首字母变成大写或小写
+	 * 
+	 * @param src 需要进行转换的字符串
+	 * @param capitalize true大写,false小写
+	 * @return 转换后的字符串
+	 */
 	private static String changeFirst(String src, boolean capitalize) {
 		if (isBlank(src)) {
 			return src;
@@ -98,6 +108,9 @@ public class StrUtils extends StringUtils {
 
 	/**
 	 * 判断一个字符串是否全是中文,包括中文标点符号等
+	 * 
+	 * @param str 需要检查的字符串
+	 * @return true:是,false:不是
 	 */
 	public static boolean isChinese(String str) {
 		char[] array = str.toCharArray();
@@ -110,13 +123,11 @@ public class StrUtils extends StringUtils {
 	}
 
 	/**
-	 * 判断是否为中文字符,包括标点等 CJK的意思是“Chinese，Japanese，Korea”的简写
-	 * ，实际上就是指中日韩三国的象形文字的Unicode编码 CJK_UNIFIED_IDEOGRAPHS:4E00-9FBF:CJK 统一表意符号
-	 * CJK_COMPATIBILITY_IDEOGRAPHS:F900-FAFF:CJK 兼容象形文字
+	 * 判断是否为中文字符,包括标点等 CJK的意思是“Chinese，Japanese，Korea”的简写 ，实际上就是指中日韩三国的象形文字的Unicode编码
+	 * CJK_UNIFIED_IDEOGRAPHS:4E00-9FBF:CJK 统一表意符号 CJK_COMPATIBILITY_IDEOGRAPHS:F900-FAFF:CJK 兼容象形文字
 	 * CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A:3400-4DBF:CJK 统一表意符号扩展A
 	 * CJK_SYMBOLS_AND_PUNCTUATION:3000-303F:CJK 符号和标点
-	 * HALFWIDTH_AND_FULLWIDTH_FORMS:FF00-FFEF:半角及全角形式
-	 * GENERAL_PUNCTUATION:2000-206F:常用标点
+	 * HALFWIDTH_AND_FULLWIDTH_FORMS:FF00-FFEF:半角及全角形式 GENERAL_PUNCTUATION:2000-206F:常用标点
 	 */
 	public static boolean isChinese(char c) {
 		Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
@@ -147,15 +158,33 @@ public class StrUtils extends StringUtils {
 
 	/**
 	 * 判断一个字符串中是否含有另外一个字符串,不区分大小写
+	 * 
+	 * @param src 被检查的字符串
+	 * @param des 需要进行检查的字符串
+	 * @return true包含
 	 */
 	public static boolean contains(CharSequence src, CharSequence des) {
 		return indexOf(src, des) > -1;
 	}
 
 	/**
-	 * @apiNote 将带下划线的字段名(蛇底式)变成驼峰式
+	 * 将带下划线的字段名(蛇底式)变成小驼峰
+	 * 
+	 * @param str 需要进行转换的字符串
+	 * @return 转换后的字符串
 	 */
 	public static String snake2Hump(String str) {
+		return snake2Hump(str, false);
+	}
+
+	/**
+	 * 将带下划线的字段名(蛇底式)变成驼峰
+	 * 
+	 * @param str 需要进行转换的字符串
+	 * @param flag true大驼峰,false小驼峰
+	 * @return 转换后的字符串
+	 */
+	public static String snake2Hump(String str, boolean flag) {
 		str = str.toLowerCase();
 		Matcher matcher = linePattern.matcher(str);
 		StringBuffer sb = new StringBuffer();
@@ -163,11 +192,12 @@ public class StrUtils extends StringUtils {
 			matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
 		}
 		matcher.appendTail(sb);
-		return changeFirst(sb.toString(), false);
+		return changeFirst(sb.toString(), flag);
 	}
 
 	/**
-	 * @apiNote 将驼峰转成蛇底式
+	 * 将驼峰转成蛇底式
+	 * 
 	 * @param str 需要进行转换的字符串
 	 * @return 转换后的字符串
 	 */
@@ -176,7 +206,8 @@ public class StrUtils extends StringUtils {
 	}
 
 	/**
-	 * @apiNote 将驼峰转成蛇底式
+	 * 将驼峰转成蛇底式
+	 * 
 	 * @param str 需要进行转换的字符串
 	 * @param lowerFirst 第一个字符串是否小写,true小写,false不管
 	 * @return 转换后的字符串
@@ -194,6 +225,9 @@ public class StrUtils extends StringUtils {
 
 	/**
 	 * 判断一个字符串是否能转成数字
+	 * 
+	 * @param cs 需要进行判断的字符
+	 * @return true能,false否
 	 */
 	public static boolean isNumeric(CharSequence cs) {
 		if (isBlank(cs)) {
